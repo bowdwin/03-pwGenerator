365104f13695ee3bd6360039f9188976dcc518c1
Keith Beaudoin <bowdwin.muninted@gmail.com>
2020-04-13 17:09:31 -0400

    having i working where it can generate 1 character based on boxes checked, now need to loop

--- jsscript.js
+++ jsscript.js
@@ -1,24 +1,107 @@
 
+//variables to grab elements to use to see if checked
+var numericCheckboxid = document.getElementById("numericCheckbox");
+var speciaCharCheckboxid = document.getElementById("speciaCharCheckbox");
+var uppercaseCheckboxid = document.getElementById("uppercaseCheckbox");
+var lowercaseCheckboxid = document.getElementById("lowercaseCheckbox");
 
-function getNumeric() {
-    return Math.floor((Math.random() * 10) + 1);
+var numberOfCharForPW = 15;
+
+//variables to check if the boxes are checked and store results
+var upperCheckboxidChecked = numericCheckbox.checked;
+var speciaCharCheckboxidChecked = speciaCharCheckboxid.checked;
+var uppercaseCheckboxidChecked = uppercaseCheckboxid.checked;
+var lowercaseCheckboxidChecked = lowercaseCheckboxid.checked;
+
+
+//console log to check if true or false
+console.log(upperCheckboxidChecked);
+console.log(speciaCharCheckboxidChecked);
+console.log(uppercaseCheckboxidChecked);
+console.log(lowercaseCheckboxidChecked);
+
+function getNumeric(checkifChecked) {
+    if (checkifChecked === true) {
+        console.log("Numeric True");
+        return Math.floor((Math.random() * 10) + 1);
+    }
+    else {
+        return false;
+    }
 }
 
-function specialCharfunc() {
+function specialCharfunc(checkifChecked) {
     var specChar = ["!", "@", "#", "$", "%", "^", "*", "(", ")"];
-    return specChar[Math.floor(Math.random() * specChar.length)];
 
-}
-function uppercase() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 129);
-}
-function lowercase() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-    console.log(lowercase)
-}
-
-function generatePw {
-    return
+    if (checkifChecked === false) {
+        console.log("Special Char True");
+        return specChar[Math.floor(Math.random() * specChar.length)];
+    }
+    else {
+        return false;
+    }
 }
 
-console.log(specialCharfunc());
+function uppercase(checkifChecked) {
+    if (checkifChecked === false) {
+        console.log("uppercase True");
+        return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    }
+    else {
+        return false;
+    }
+}
+
+function lowercase(checkifChecked) {
+    if (checkifChecked === true) {
+        console.log("Lowercase True");
+        return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    }
+    else {
+        return false;
+    }
+}
+
+
+function generatepw(a, b, c, d, ) {
+
+    var listOfItems = [a, b, c, d];
+    var listToRandomize = [];
+    var finalPw = "";
+
+
+    for (var i = 0; i < listOfItems.length; i++) {
+        if (listOfItems[i] === false) {
+            //do nothing
+            console.log(listOfItems[i]);
+        }
+        else {
+            //add to list
+            var listToRandomize = listToRandomize.concat(listOfItems[i]);
+            console.log(listToRandomize);
+        }
+    }
+    finalPw = finalPw.concat(listToRandomize[Math.floor(Math.random() * listToRandomize.length)]);
+    console.log(finalPw + " this is final pw");
+    // return (listToRandomize[Math.floor(Math.random() * listToRandomize.length)])
+    return finalPw;
+}
+
+
+// console.log(getNumeric(upperCheckboxidChecked));
+// console.log(specialCharfunc(speciaCharCheckboxidChecked));
+// console.log(uppercase(uppercaseCheckboxidChecked));
+// console.log(lowercase(lowercaseCheckboxidChecked));
+
+var numericResults = (getNumeric(upperCheckboxidChecked));
+var specialCharResults = (specialCharfunc(speciaCharCheckboxidChecked));
+var uppercaseResults = (uppercase(uppercaseCheckboxidChecked));
+var lowercaseResuts = (lowercase(lowercaseCheckboxidChecked));
+
+console.log(numericResults);
+console.log(specialCharResults);
+console.log(uppercaseResults);
+console.log(lowercaseResuts);
+
+console.log(generatepw(numericResults, specialCharResults, uppercaseResults, lowercaseResuts));
+
